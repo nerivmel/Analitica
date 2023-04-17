ff61e7063100c34de96801f893885d3f16368619
Wilmer Soto <wilmer.soto@udea.edu.co>
2023-04-16 20:36:19 -0500

    Entidades base de datos agregadas.

--- Backend/src/main/java/com/example/Analitica/entity/Materias_Plan.java
+++ Backend/src/main/java/com/example/Analitica/entity/Materias_Plan.java
@@ -1,4 +1,30 @@
 package com.example.Analitica.entity;
 
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "materias_plan")
+@EntityListeners(AuditingEntityListener.class)
 public class Materias_Plan {
+    private long id;
+    private String nivel;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    public long getId() {
+        return id;
+    }
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    @Column(name = "nivel", nullable = false)
+    public String getNivel() {
+        return nivel;
+    }
+    public void setNivel(String nivel) {
+        this.nivel = nivel;
+    }
 }
--- Backend/src/main/java/com/example/Analitica/entity/Requisito.java
+++ Backend/src/main/java/com/example/Analitica/entity/Requisito.java
@@ -1,4 +1,42 @@
 package com.example.Analitica.entity;
 
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "requisito")
+@EntityListeners(AuditingEntityListener.class)
 public class Requisito {
+    private long id;
+    private String tipoRequisito;
+    private String estado;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    public long getId() {
+        return id;
+    }
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    @Column(name = "tipo_requisito", nullable = false)
+    public String getTipoRequisito() {
+        return tipoRequisito;
+    }
+    public void setTipoRequisito(String tipoRequisito) {
+        this.tipoRequisito = tipoRequisito;
+    }
+
+    @Column(name = "estado", nullable = false)
+    public String getEstado() {
+        return estado;
+    }
+    public void setEstado(String estado) {
+        this.estado = estado;
+    }
+
 }
+
+
