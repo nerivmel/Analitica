da6bba7b14abe79cfc1d01001198ddc4ee7f25a0
Emanuel López <97258179+EmanuelLopezH@users.noreply.github.com>
2023-05-07 14:29:39 -0500

    Creación del drag and drop

---
+++ frontend/components/DragDrop.js
@@ -0,0 +1,64 @@
+import { useState, useRef } from "react"
+import Image from 'next/image';
+import imagen from '../images/icons8-documento-64.png';
+
+
+const DragDrop = () => {
+    const [file, setFiles] = useState(null)
+    const inputRef = useRef();
+    
+
+    const handleDragOver = (event) => {
+        event.preventDefault();
+        console.log("dragging over")
+    }
+
+    const handleDrop = (event) => {
+        event.preventDefault();
+        setFiles(event.dataTransfer.files)
+    }   
+
+    const handleUpload = () => { };
+
+    if(file) return (
+        <div className="uploads">
+            <ul>
+                {Array.from(file).map((file, idx) => <li key={idx}>{file.name}</li> )}
+            </ul>
+            <div className="actions">
+                <button onClick={() => setFiles(null)}>Cancelar</button>
+                <button onClick={handleUpload}>Subir</button>
+            </div>
+        </div>
+    )
+
+    return (
+        <>
+        {!file && (
+
+            <div className="dropzone"
+                onDragOver={handleDragOver}
+                onDrop={handleDrop}
+            >
+                <Image className="img1" src={imagen} />
+                <h1 className="titulo-drag">Arrastre y suelte archivos</h1>
+                <h1 className="td">O</h1>
+
+                <input 
+                    type="file"
+                    multiple
+                    onChange={(event) => setFiles(event.target.file)}
+                    hidden
+                    ref={inputRef}
+                  />
+                <button  className="tb" onClick={() => inputRef.current.click()}>Seleccione archivos</button>
+
+            </div>
+        )}
+        </>
+
+    )
+
+};
+
+export default DragDrop
