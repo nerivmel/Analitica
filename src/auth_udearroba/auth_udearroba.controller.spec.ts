183a09c7af83925446473d8fe61c492cc870d1d0
Manuel Calle <manuel.calle@udea.edu.co>
2023-05-12 13:37:21 -0500

    Creando pruebas unitarias y de integracion

--- src/auth_udearroba/auth_udearroba.controller.spec.ts
+++ src/auth_udearroba/auth_udearroba.controller.spec.ts
@@ -1,6 +1,10 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { AuthUdearrobaController } from './auth_udearroba.controller';
 import { AuthUdearrobaService } from './auth_udearroba.service';
+import { CreateAuthUdearrobaDto } from './dto/create-auth_udearroba.dto';
+import { UpdateAuthUdearrobaDto } from './dto/update-auth_udearroba.dto';
+import { LoginAuthUdearrobaDto } from './dto/login-auth_udearroba.dto';
+import { HttpException, HttpStatus } from '@nestjs/common';
 
 describe('AuthUdearrobaController', () => {
   let controller: AuthUdearrobaController;
@@ -18,8 +22,195 @@ describe('AuthUdearrobaController', () => {
 
     controller = module.get<AuthUdearrobaController>(AuthUdearrobaController);
   });
+  it('test_create_new_user_with_valid_input', async () => {
+    const mockCreateAuthUdearrobaDto = new CreateAuthUdearrobaDto();
+    mockCreateAuthUdearrobaDto.nombre = 'John Doe';
+    mockCreateAuthUdearrobaDto.documento = '123456789';
+    mockCreateAuthUdearrobaDto.email = 'johndoe@example.com';
+    mockCreateAuthUdearrobaDto.password = 'password';
+    mockCreateAuthUdearrobaDto.area = 'IT';
+    mockCreateAuthUdearrobaDto.cargo = 'Developer';
+
+    const mockAuthUdearrobaService = {
+      create: jest.fn().mockResolvedValue({
+        message: 'Usuario AuthUdea@ creado correctamente',
+      }),
+    };
+
+    const authUdearrobaController = new AuthUdearrobaController(
+      mockAuthUdearrobaService as any,
+    );
+
+    const result = await authUdearrobaController.create(
+      mockCreateAuthUdearrobaDto,
+    );
+
+    expect(mockAuthUdearrobaService.create).toHaveBeenCalledWith(
+      mockCreateAuthUdearrobaDto,
+    );
+    expect(result).toEqual({
+      message: 'Usuario AuthUdea@ creado correctamente',
+    });
+  });
+  it('test_find_all_users', async () => {
+    const mockAuthUdearrobaService = {
+      findAll: jest.fn().mockResolvedValue([
+        { id: 1, nombre: 'John Doe' },
+        { id: 2, nombre: 'Jane Doe' },
+      ]),
+    };
+
+    const authUdearrobaController = new AuthUdearrobaController(
+      mockAuthUdearrobaService as any,
+    );
+
+    const result = await authUdearrobaController.findAll();
+
+    expect(mockAuthUdearrobaService.findAll).toHaveBeenCalled();
+    expect(result).toEqual([
+      { id: 1, nombre: 'John Doe' },
+      { id: 2, nombre: 'Jane Doe' },
+    ]);
+  });
+  it('test_find_user_by_id', async () => {
+    const mockAuthUdearrobaService = {
+      findOne: jest.fn().mockResolvedValue({ id: 1, nombre: 'John Doe' }),
+    };
+
+    const authUdearrobaController = new AuthUdearrobaController(
+      mockAuthUdearrobaService as any,
+    );
+
+    const result = await authUdearrobaController.findOne('1');
+
+    expect(mockAuthUdearrobaService.findOne).toHaveBeenCalledWith(1);
+    expect(result).toEqual({ id: 1, nombre: 'John Doe' });
+  });
+
+  it('test_update_user_with_valid_input', async () => {
+    const mockUpdateAuthUdearrobaDto = new UpdateAuthUdearrobaDto();
+    mockUpdateAuthUdearrobaDto.nombre = 'John Doe';
+    mockUpdateAuthUdearrobaDto.documento = '123456789';
+    mockUpdateAuthUdearrobaDto.email = 'johndoe@example.com';
+    mockUpdateAuthUdearrobaDto.password = 'password';
+    mockUpdateAuthUdearrobaDto.area = 'IT';
+    mockUpdateAuthUdearrobaDto.cargo = 'Developer';
+
+    const mockAuthUdearrobaService = {
+      update: jest
+        .fn()
+        .mockResolvedValue({ message: 'Usuario Actualizado correctamente' }),
+    };
+
+    const authUdearrobaController = new AuthUdearrobaController(
+      mockAuthUdearrobaService as any,
+    );
+
+    const result = await authUdearrobaController.update(
+      '1',
+      mockUpdateAuthUdearrobaDto,
+    );
+
+    expect(mockAuthUdearrobaService.update).toHaveBeenCalledWith(
+      1,
+      mockUpdateAuthUdearrobaDto,
+    );
+    expect(result).toEqual({ message: 'Usuario Actualizado correctamente' });
+  });
+  it('test_remove_user_by_id', async () => {
+    const mockAuthUdearrobaService = {
+      remove: jest
+        .fn()
+        .mockResolvedValue({ message: 'Usuario eliminado correctamente' }),
+    };
+
+    const authUdearrobaController = new AuthUdearrobaController(
+      mockAuthUdearrobaService as any,
+    );
+
+    const result = await authUdearrobaController.remove('1');
+
+    expect(mockAuthUdearrobaService.remove).toHaveBeenCalledWith(1);
+    expect(result).toEqual({ message: 'Usuario eliminado correctamente' });
+  });
+  it('test_login_with_valid_credentials', async () => {
+    const mockLoginAuthUdearrobaDto = new LoginAuthUdearrobaDto();
+    mockLoginAuthUdearrobaDto.email = 'johndoe@example.com';
+    mockLoginAuthUdearrobaDto.password = 'password';
+
+    const mockAuthUdearrobaService = {
+      login: jest.fn().mockResolvedValue({
+        message: 'Bienvenido',
+        result: 'success',
+        nombre: 'John Doe',
+        sub: 'johndoe@example.com',
+        token: 'token',
+      }),
+    };
+
+    const authUdearrobaController = new AuthUdearrobaController(
+      mockAuthUdearrobaService as any,
+    );
+
+    const result = await authUdearrobaController.login(
+      mockLoginAuthUdearrobaDto,
+    );
+
+    expect(mockAuthUdearrobaService.login).toHaveBeenCalledWith(
+      mockLoginAuthUdearrobaDto,
+    );
+    expect(result).toEqual({
+      message: 'Bienvenido',
+      result: 'success',
+      nombre: 'John Doe',
+      sub: 'johndoe@example.com',
+      token: 'token',
+    });
+  });
 
   it('should be defined', () => {
     expect(controller).toBeDefined();
+  });
+});
+describe('AuthUdearrobaController', () => {
+  let controller: AuthUdearrobaController;
+
+  beforeEach(async () => {
+    const module: TestingModule = await Test.createTestingModule({
+      controllers: [AuthUdearrobaController],
+      providers: [
+        {
+          provide: AuthUdearrobaService,
+          useValue: {
+            create: jest
+              .fn()
+              .mockRejectedValue(
+                new HttpException(
+                  'El email ingresado ya se encuentra registrado',
+                  200,
+                ),
+              ),
+          },
+        },
+      ],
+    }).compile();
+
+    controller = module.get<AuthUdearrobaController>(AuthUdearrobaController);
+  });
+  // Tests creating a new user with an existing email.
+  it('test_create_user_with_existing_email', () => {
+    const createAuthUdearrobaDto = new CreateAuthUdearrobaDto();
+    createAuthUdearrobaDto.nombre = 'John Doe';
+    createAuthUdearrobaDto.documento = '123456789';
+    createAuthUdearrobaDto.email = 'johndoe@example.com';
+    createAuthUdearrobaDto.password = 'password';
+    createAuthUdearrobaDto.area = 'IT';
+    createAuthUdearrobaDto.cargo = 'Developer';
+    expect(controller.create(createAuthUdearrobaDto)).rejects.toThrow(
+      new HttpException(
+        'El email ingresado ya se encuentra registrado',
+        HttpStatus.CONFLICT,
+      ),
+    );
   });
 });
--- src/auth_udearroba/auth_udearroba.service.spec.ts
+++ src/auth_udearroba/auth_udearroba.service.spec.ts
@@ -1,7 +1,11 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { AuthUdearrobaService } from './auth_udearroba.service';
+import { CreateAuthUdearrobaDto } from './dto/create-auth_udearroba.dto';
+import { HttpException } from '@nestjs/common';
+import { UpdateAuthUdearrobaDto } from './dto/update-auth_udearroba.dto';
+import { LoginAuthUdearrobaDto } from './dto/login-auth_udearroba.dto';
 
-describe('AuthUdearrobaService', () => {
+describe('AuthUdearrobaService created successfully', () => {
   let service: AuthUdearrobaService;
 
   beforeEach(async () => {
@@ -9,15 +13,267 @@ describe('AuthUdearrobaService', () => {
       providers: [
         {
           provide: AuthUdearrobaService,
-          useValue: {},
+          useValue: {
+            findOne: jest.fn().mockReturnValue(null),
+            create: jest.fn(),
+            signAsync: jest.fn().mockReturnValue('mockToken'),
+          },
         },
       ],
     }).compile();
 
     service = module.get<AuthUdearrobaService>(AuthUdearrobaService);
   });
-
+  // Tests that a new user can be created successfully.
+  it('test_create_user_successfully', async () => {
+    const createAuthUdearrobaDto = new CreateAuthUdearrobaDto();
+    createAuthUdearrobaDto.documento = '123456789';
+    createAuthUdearrobaDto.nombre = 'John Doe';
+    createAuthUdearrobaDto.email = 'johndoe@example.com';
+    createAuthUdearrobaDto.password = 'password';
+    createAuthUdearrobaDto.area = 'IT';
+    createAuthUdearrobaDto.cargo = 'Developer';
+    await service.create(createAuthUdearrobaDto);
+    expect(service.findOne).toHaveBeenCalledTimes(0);
+    expect(service.create).toHaveBeenCalledTimes(1);
+  });
   it('should be defined', () => {
     expect(service).toBeDefined();
+  });
+});
+describe('AuthUdearrobaService find all users', () => {
+  let service: AuthUdearrobaService;
+  beforeEach(async () => {
+    const module: TestingModule = await Test.createTestingModule({
+      providers: [
+        {
+          provide: AuthUdearrobaService,
+          useValue: {
+            findAll: jest.fn().mockReturnValue([
+              { id: 1, nombre: 'John Doe' },
+              { id: 2, nombre: 'Jane Doe' },
+            ]),
+            signAsync: jest.fn(),
+          },
+        },
+      ],
+    }).compile();
+    service = module.get<AuthUdearrobaService>(AuthUdearrobaService);
+  });
+  it('test_find_all_users_successfully', async () => {
+    const result = await service.findAll();
+    expect(result).toEqual([
+      { id: 1, nombre: 'John Doe' },
+      { id: 2, nombre: 'Jane Doe' },
+    ]);
+  });
+});
+describe('AuthUdearrobaService update non existent user', () => {
+  let service: AuthUdearrobaService;
+  beforeEach(async () => {
+    const module: TestingModule = await Test.createTestingModule({
+      providers: [
+        {
+          provide: AuthUdearrobaService,
+          useValue: {
+            update: jest.fn().mockRejectedValue(new HttpException('💣', 400)),
+            findOne: jest.fn().mockReturnValue(null),
+            create: jest.fn(),
+            findAll: jest.fn().mockReturnValue([]),
+            signAsync: jest.fn(),
+          },
+        },
+      ],
+    }).compile();
+    service = module.get<AuthUdearrobaService>(AuthUdearrobaService);
+  });
+  // Tests that an error is thrown when trying to update a non-existing user.
+  it('test_update_non_existing_user', async () => {
+    const updateAuthUdearrobaDto = new UpdateAuthUdearrobaDto();
+    updateAuthUdearrobaDto.documento = '123456789';
+    updateAuthUdearrobaDto.nombre = 'John Doe';
+    updateAuthUdearrobaDto.email = 'johndoe@example.com';
+    updateAuthUdearrobaDto.password = 'password';
+    updateAuthUdearrobaDto.area = 'IT';
+    updateAuthUdearrobaDto.cargo = 'Developer';
+    await expect(service.update(1, updateAuthUdearrobaDto)).rejects.toThrow(
+      HttpException,
+    );
+  });
+});
+describe('AuthUdearrobaService remove', () => {
+  let service: AuthUdearrobaService;
+  beforeEach(async () => {
+    const module: TestingModule = await Test.createTestingModule({
+      providers: [
+        {
+          provide: AuthUdearrobaService,
+          useValue: {
+            remove: jest
+              .fn()
+              .mockRejectedValue(
+                new HttpException('removido exitosamnete', 200),
+              ),
+            findOne: jest.fn().mockReturnValue(null),
+            create: jest.fn(),
+            findAll: jest.fn().mockReturnValue([]),
+            signAsync: jest.fn(),
+          },
+        },
+      ],
+    }).compile();
+    service = module.get<AuthUdearrobaService>(AuthUdearrobaService);
+  });
+  // Tests that an error is thrown when trying to remove a non-existing user.
+  it('test_remove_non_existing_user', async () => {
+    await expect(service.remove(1)).rejects.toThrow(HttpException);
+  });
+});
+describe('AuthUdearrobaService test wrong email or pass', () => {
+  let service: AuthUdearrobaService;
+  beforeEach(async () => {
+    const module: TestingModule = await Test.createTestingModule({
+      providers: [
+        {
+          provide: AuthUdearrobaService,
+          useValue: {
+            login: jest
+              .fn()
+              .mockRejectedValue(
+                new HttpException('removido exitosamnete', 200),
+              ),
+            findOne: jest.fn().mockReturnValue(null),
+            create: jest.fn(),
+            findAll: jest.fn().mockReturnValue([]),
+            signAsync: jest.fn(),
+          },
+        },
+      ],
+    }).compile();
+    service = module.get<AuthUdearrobaService>(AuthUdearrobaService);
+  });
+  // Tests that an error is thrown when trying to login with incorrect email or password.
+  it('test_login_with_incorrect_email_or_password', async () => {
+    const loginAuthUdearrobaDto = new LoginAuthUdearrobaDto();
+    loginAuthUdearrobaDto.email = 'johndoe@example.com';
+    loginAuthUdearrobaDto.password = 'password';
+    await expect(service.login(loginAuthUdearrobaDto)).rejects.toThrow(
+      HttpException,
+    );
+  });
+});
+describe('AuthUdearrobaService find one user', () => {
+  let service: AuthUdearrobaService;
+  beforeEach(async () => {
+    const module: TestingModule = await Test.createTestingModule({
+      providers: [
+        {
+          provide: AuthUdearrobaService,
+          useValue: {
+            findOne: jest.fn().mockReturnValue({ id: 1, nombre: 'John Doe' }),
+            create: jest.fn(),
+            findAll: jest.fn().mockReturnValue([]),
+            signAsync: jest.fn(),
+          },
+        },
+      ],
+    }).compile();
+    service = module.get<AuthUdearrobaService>(AuthUdearrobaService);
+  });
+  // Tests that one user can be found successfully.
+  it('test_find_one_user_successfully', async () => {
+    const result = await service.findOne(1);
+    expect(result).toEqual({ id: 1, nombre: 'John Doe' });
+  });
+});
+describe('AuthUdearrobaService create user with existent doc', () => {
+  let service: AuthUdearrobaService;
+  beforeEach(async () => {
+    const module: TestingModule = await Test.createTestingModule({
+      providers: [
+        {
+          provide: AuthUdearrobaService,
+          useValue: {
+            findOne: jest.fn().mockReturnValue({}),
+            create: jest
+              .fn()
+              .mockRejectedValue(new HttpException('documento duplicado', 200)),
+            findAll: jest.fn().mockReturnValue([]),
+            signAsync: jest.fn(),
+          },
+        },
+      ],
+    }).compile();
+    service = module.get<AuthUdearrobaService>(AuthUdearrobaService);
+  });
+  // Tests that an error is thrown when trying to create a user with an existing document number.
+  it('test_create_user_with_existing_document_number', async () => {
+    const createAuthUdearrobaDto = new CreateAuthUdearrobaDto();
+    createAuthUdearrobaDto.documento = '123456789';
+    createAuthUdearrobaDto.nombre = 'John Doe';
+    createAuthUdearrobaDto.email = 'johndoe@example.com';
+    createAuthUdearrobaDto.password = 'password';
+    createAuthUdearrobaDto.area = 'IT';
+    createAuthUdearrobaDto.cargo = 'Developer';
+    await expect(service.create(createAuthUdearrobaDto)).rejects.toThrow(
+      HttpException,
+    );
+  });
+});
+describe('AuthUdearrobaService update', () => {
+  let service: AuthUdearrobaService;
+  beforeEach(async () => {
+    const module: TestingModule = await Test.createTestingModule({
+      providers: [
+        {
+          provide: AuthUdearrobaService,
+          useValue: {
+            update: jest.fn().mockReturnValue({
+              message: 'Usuario Actualizado correctamente',
+            }),
+            findOne: jest.fn().mockReturnValue({ update: jest.fn() }),
+          },
+        },
+      ],
+    }).compile();
+    service = module.get<AuthUdearrobaService>(AuthUdearrobaService);
+  });
+  // Tests that a user can be updated successfully.
+  it('test_update_user_successfully', async () => {
+    const updateAuthUdearrobaDto = new UpdateAuthUdearrobaDto();
+    updateAuthUdearrobaDto.documento = '123456789';
+    updateAuthUdearrobaDto.nombre = 'John Doe';
+    updateAuthUdearrobaDto.email = 'johndoe@example.com';
+    updateAuthUdearrobaDto.password = 'password';
+    updateAuthUdearrobaDto.area = 'IT';
+    updateAuthUdearrobaDto.cargo = 'Developer';
+    const result = await service.update(1, updateAuthUdearrobaDto);
+    expect(service.findOne).toHaveBeenCalledTimes(0);
+    expect(result).toEqual({ message: 'Usuario Actualizado correctamente' });
+  });
+});
+describe('AuthUdearrobaService delete', () => {
+  let service: AuthUdearrobaService;
+  beforeEach(async () => {
+    const module: TestingModule = await Test.createTestingModule({
+      providers: [
+        {
+          provide: AuthUdearrobaService,
+          useValue: {
+            remove: jest
+              .fn()
+              .mockReturnValue({ message: 'Usuario eliminado correctamente' }),
+            findOne: jest.fn().mockReturnValue({ destroy: jest.fn() }),
+          },
+        },
+      ],
+    }).compile();
+    service = module.get<AuthUdearrobaService>(AuthUdearrobaService);
+  });
+  // Tests that a user can be removed successfully.
+  it('test_remove_user_successfully', async () => {
+    const result = await service.remove(1);
+    expect(service.findOne).toHaveBeenCalledTimes(0);
+    expect(result).toEqual({ message: 'Usuario eliminado correctamente' });
   });
 });
